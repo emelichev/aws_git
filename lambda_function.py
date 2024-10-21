203a37c2843b6582784015e185625eb3b749ef5f
Denis Astahov <adv4000@gmail.com>
2024-10-21 23:42:15 +0000

    Upload New File

---
+++ lambda_function.py
@@ -0,0 +1,29 @@
+import sys
+import json
+import datetime
+import pybase64
+
+
+def lambda_handler(event, context):
+    datetime_now     = datetime.datetime.now()
+    current_datetime = str(datetime_now.strftime("%d-%m-%Y %H:%M:%S"))
+    
+    if 'body' not in event:
+        PAYLOAD = "NOT PROVIDED"
+    else:
+        PAYLOAD = str(pybase64.b64decode(event['body']),'utf-8')
+        
+    response = {
+        "statusCode": 200,
+        "headers"   : { "Content-Type": "application/json" },
+        "body": json.dumps({
+            "APIVersion": "v1.0",
+            "Message"   : "AWS Lambda with Docker and Python!",
+            "Developer" : "Denis Astahov",
+            "Python"    : str(sys.version),
+            "TimeStamp" : current_datetime,
+            "Payload"   : PAYLOAD
+        })
+    }
+
+    return response
